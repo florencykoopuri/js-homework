fa1af0f2617129d0ac5980785427646f656250bb
Florency Koopuri <qogu@banit.club>
2017-07-20 15:07:08 -0400

    Create User compoennt created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LogoutComponent } from './account/logout.component';
-import { UserDetailComponent } from './user/detail.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {LogoutComponent} from './account/logout.component';
+import {UserDetailComponent} from './user/detail.component';
+import {CreateUserComponent} from './create-user/create-user.component';
 
 const mainRoutes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'logout', component: LogoutComponent },
-  { path: 'users/:id', component: UserDetailComponent }
+  {path: '', component: DashboardComponent},
+  {path: 'logout', component: LogoutComponent},
+  {path: 'users/:id', component: UserDetailComponent},
+  {path: 'createuser', component: CreateUserComponent}
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(mainRoutes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(mainRoutes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
