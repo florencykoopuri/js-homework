65f6821448ca005a7d824c8f14a385fc6d708681
Florency Koopuri <qogu@banit.club>
2017-07-20 15:36:36 -0400

    Added validation.

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../user.service';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+  styleUrls: ['./create-user.component.css'],
+  providers: [UserService]
 })
 export class CreateUserComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+  public serverResponse: any;
+
+  constructor(private userService : UserService) { }
 
   ngOnInit() {
+    this.serverResponse = this.userService.getServerResponse();
+  }
+
+  createUser() {
+
   }
 
 }
